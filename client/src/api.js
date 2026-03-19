51251a21d58fbb77e9779c685299484c17a4933e
RedzoEbad <ebadkhan6292002@gmail.com>
2026-03-20 04:23:45 +0500

    updated code

--- client/src/api.js
+++ client/src/api.js
@@ -1,8 +1,7 @@
-const API = 'http://localhost:5000';
-
+const API = "/";
 export async function fetchItems() {
   try {
-    const res = await fetch(`${API}/api/items`);
+    const res = await fetch(`${API}api/items`);
     return await res.json();
   } catch (err) {
     console.error(err);
@@ -11,7 +10,7 @@ export async function fetchItems() {
 }
 
 export async function createItem(payload) {
-  const res = await fetch(`${API}/api/items`, {
+  const res = await fetch(`${API}api/items`, {
     method: 'POST',
     headers: { 'Content-Type': 'application/json' },
     body: JSON.stringify(payload)
@@ -20,6 +19,6 @@ export async function createItem(payload) {
 }
 
 export async function deleteItem(id) {
-  const res = await fetch(`${API}/api/items/${id}`, { method: 'DELETE' });
+  const res = await fetch(`${API}api/items/${id}`, { method: 'DELETE' });
   return await res.json();
 }
